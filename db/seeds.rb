cb830e98ab8864ff0fd04af42da82031750e2398
MarkXtenda <mark.i2p.1998@gmail.com>
2022-12-06 15:57:45 +1100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p1 = Passenger.create(name: "Bro")
+
+t1 = Taxi.create(name: "Deliver")
+
+ride = Ride.create(taxi_id: t1.id, passenger_id: p1.id)
